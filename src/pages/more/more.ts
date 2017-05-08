4c7f3bfc739d698a5c5cb77b3a007d73b6945953
HAN <hmy2009@126.com>
2017-05-08 22:43:54 +0800

    更多页面

--- src/pages/more/more.ts
+++ src/pages/more/more.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
 
+import { Slides } from 'ionic-angular';
 @Component({
-   selector: 'page-more',
-   templateUrl: 'more.html'
+    selector: 'page-more',
+    templateUrl: 'more.html'
 })
 export class MorePage {
+    @ViewChild(Slides) slides: Slides;
+    public slidesList =[
+        {title:'第一藏独',image:'http://f12.baidu.com/it/u=218023967,2981160963&fm=76'},
+        {title:'第二张图',image:'http://f12.baidu.com/it/u=501933761,3688807382&fm=76'}];
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
+    goToSlide() {
+        console.log(this.slides);
+        this.slides.slideTo(2, 500);
+    }
 
 }
+
