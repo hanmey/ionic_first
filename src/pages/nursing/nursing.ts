4778c1b0cb38758d1e05fc902a0f6ccac51b012b
hanmey <hmy2009@126.com>
2017-05-09 16:13:34 +0800

    '提交表单'

--- src/pages/nursing/nursing.ts
+++ src/pages/nursing/nursing.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +7,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'nursing.html'
 })
 export class NursingPage {
+  @ViewChild('signupSlider') signupSlider: any;
+  slideOneForm: FormGroup;
+  submitAttempt: boolean = false;
+  constructor(public navCtrl: NavController,public formBuilder: FormBuilder) {
+      this.slideOneForm = formBuilder.group({
+        name: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        password: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+      });
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  SubmitData(){
+    console.log(this.slideOneForm.value);
   }
 
 }
---
+++ src/providers/FormService.ts
@@ -0,0 +1,9 @@
+import {Injectable} from '@angular/core';
+import {Response, Headers, RequestOptions, URLSearchParams, RequestOptionsArgs} from '@angular/http';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+@Injectable()
+export class FormService {
+  constructor() {
+  }
+}
