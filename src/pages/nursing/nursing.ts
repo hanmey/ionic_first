9615823891e8ed68001050d6e99a307f0ac950f7
hanmey <hmy2009@126.com>
2017-05-10 16:20:18 +0800

    form submit

--- src/pages/nursing/nursing.ts
+++ src/pages/nursing/nursing.ts
@@ -7,18 +7,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'nursing.html'
 })
 export class NursingPage {
-  @ViewChild('signupSlider') signupSlider: any;
   slideOneForm: FormGroup;
   submitAttempt: boolean = false;
   constructor(public navCtrl: NavController,public formBuilder: FormBuilder) {
       this.slideOneForm = formBuilder.group({
-        name: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-        password: ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+        name: ['Nancy',Validators.compose([Validators.maxLength(30),Validators.minLength(2),Validators.required, Validators.pattern('[a-zA-Z ]*')])],
+        password: ['',Validators.compose([Validators.maxLength(18), Validators.minLength(6),Validators.required, Validators.pattern('[a-zA-Z ]*')])]
+      },function(g:FormGroup){
+            console.log('dsfsdfsdfsd');
+            console.log('callback');
+            return false;
       });
-
+      formBuilder.control(this.slideOneForm);
   }
-  SubmitData(){
-    console.log(this.slideOneForm.value);
+  submitData(){
+      console.log(this.formBuilder.control(this.slideOneForm.value));
+    return false;
+  }
+  validForm(){
+
   }
 
 }
